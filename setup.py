b5ad27479c1f21fd31aa5ba28f65f0ace54c7ad9
Carlos <carlosspy@gmail.com>
2022-11-25 19:54:55 +0000

    hello

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="code_challenge",
+    version='0.1.0',
+    description='A stock recommendation package',
+    author="Carlos",
+    author_email="carlosspy@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
