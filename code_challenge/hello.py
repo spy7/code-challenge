7b837cc7745929d87a63541a8c57e02484e0c069
Carlos <carlosspy@gmail.com>
2022-11-27 20:23:39 +0000

    config

--- code_challenge/hello.py
+++
@@ -1,2 +0,0 @@
-def hw():
-    return "Hello World!"
--- code_challenge/scripts/code_challenge.py
+++ code_challenge/scripts/code_challenge.py
@@ -12,6 +12,10 @@ def main():
     parser = argparse.ArgumentParser("code_challenge")
     command = parser.add_subparsers(dest="command")
     command.required = True
+    config = command.add_parser("config")
+    config.add_argument("-H", "--host")
+    config.add_argument("-e", "--endpoint")
+    config.add_argument("-t", "--token")
     get_companies = command.add_parser("get_companies")
     get_companies.add_argument("-s", "--sector")
     get_companies.add_argument("-c", "--country")
@@ -25,11 +29,20 @@ def main():
     get_recommendations.add_argument("-e", "--end_date")
     args = parser.parse_args()
 
-    settings = ConfigFile().read()
+    config_file = ConfigFile()
+    settings = config_file.read()
     reader = GraphQLReader(settings)
     service = CompanyService(reader)
 
     try:
+        if args.command == "config":
+            if args.host:
+                settings.host = args.host
+            if args.endpoint:
+                settings.endpoint = args.endpoint
+            if args.token:
+                settings.token = args.token
+            config_file.write(settings)       
         if args.command == "get_companies":
             sector = args.sector
             country = args.country
